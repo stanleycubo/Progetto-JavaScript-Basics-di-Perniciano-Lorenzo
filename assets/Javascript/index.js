fb2970785930e21300d5cf22d9754cac7c5d7cab
stanleycubo <tic1apfperniciano@gmail.com>
2023-04-26 17:19:42 +0200

    modifiche per il DOM

--- assets/Javascript/index.js
+++ assets/Javascript/index.js
@@ -1,17 +1,38 @@
-const counter = document.getElementById("counter");
-const incrementButton = document.getElementById("buttonRight");
-const decrementButton = document.getElementById("buttonLeft");
-const resetButton = document.getElementById("resetButton");
+const buttonLeft = document.createElement("button");
+buttonLeft.textContent = " - ";
+buttonLeft.id = "buttonLeft";
+document.getElementById("container").append(buttonLeft);
+
+const containerDiv = document.createElement("div");
+containerDiv.id = "containerDiv";
+document.getElementById("container").append(containerDiv);
+
+const counter = document.createElement("h2");
+counter.id = "counter";
+document.getElementById("containerDiv").append(counter);
+
+const resetButton = document.createElement("h2");
+resetButton.textContent = " RESET ";
+resetButton.setAttribute("role", "button");
+resetButton.id = "resetButton";
+document.getElementById("containerDiv").append(resetButton);
+
+const buttonRight = document.createElement("button");
+buttonRight.textContent = " + ";
+buttonRight.id = "buttonRight";
+document.getElementById("container").append(buttonRight);
+
+//! non so se può funzionare
 
 let count = 0;
 counter.textContent = count;
 
-incrementButton.addEventListener("click", () => {
+buttonRight.addEventListener("click", () => {
   count++;
   counter.textContent = count;
 });
 
-decrementButton.addEventListener("click", () => {
+buttonLeft.addEventListener("click", () => {
   count--;
   counter.textContent = count;
 });
