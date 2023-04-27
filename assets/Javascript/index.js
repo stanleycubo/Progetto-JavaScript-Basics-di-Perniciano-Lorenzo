0a3ca1be5e6de34f8bd73d53d078aecda7ad3729
stanleycubo <tic1apfperniciano@gmail.com>
2023-04-27 11:38:01 +0200

    sistemata formattazione js

--- assets/Javascript/index.js
+++ assets/Javascript/index.js
@@ -1,25 +1,27 @@
 const buttonLeft = document.createElement("button");
+const containerDiv = document.createElement("div");
+const counter = document.createElement("h2");
+const resetButton = document.createElement("h2");
+const buttonRight = document.createElement("button");
+
 buttonLeft.textContent = " - ";
 buttonLeft.id = "buttonLeft";
-document.getElementById("container").append(buttonLeft);
 
-const containerDiv = document.createElement("div");
 containerDiv.id = "containerDiv";
-document.getElementById("container").append(containerDiv);
 
-const counter = document.createElement("h2");
 counter.id = "counter";
-document.getElementById("containerDiv").append(counter);
 
-const resetButton = document.createElement("h2");
 resetButton.textContent = " RESET ";
 resetButton.setAttribute("role", "button");
 resetButton.id = "resetButton";
-document.getElementById("containerDiv").append(resetButton);
 
-const buttonRight = document.createElement("button");
 buttonRight.textContent = " + ";
 buttonRight.id = "buttonRight";
+
+document.getElementById("container").append(buttonLeft);
+document.getElementById("container").append(containerDiv);
+document.getElementById("containerDiv").append(counter);
+document.getElementById("containerDiv").append(resetButton);
 document.getElementById("container").append(buttonRight);
 
 let count = 0;
